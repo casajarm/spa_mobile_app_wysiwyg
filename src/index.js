caf2e281a83b7ea1cdc66356985fedc0f33ea926
Greg Jarmiolowski <greg@sqlprompt.net>
2019-02-04 11:26:50 -0700

    Removed redundant code. The www module is where the NodeJS server is initialized and set to listen.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -12,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
