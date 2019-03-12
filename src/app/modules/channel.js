91c95941334c1a68eef86dbb60515ad7d91375d7
Greg Jarmiolowski <greg@sqlprompt.net>
2019-03-12 11:46:13 -0600

    Using Parse Platform APIs where possible

--- src/app/modules/channel.js
+++ src/app/modules/channel.js
@@ -1,9 +1,14 @@
 import {Organization, getOrganization} from "./organizations.js";
 import Likemoji from './likemojis.js';
 import {ChannelStyle, saveNewStyle, getStyle} from "./styles.js";
-import {Group, getMainCategory} from "./groups.js";
+//import {Group, getMainCategory} from "./groups.js";
 
 var org = Parse.Object.extend("Organization");
+
+/* 
+removeXX means a relation is delete
+deleteXX means the object is deleted and relations are removed
+*/
 
 const Channel =  {
     channel:    new org(),
@@ -28,18 +33,40 @@ const Channel =  {
         return this.likemojis.filter((moji) => catMojis.find(x => x === moji.id));
     },
     
-    removeCategory: function(category) {
-        // remove from the channel object too
+    deleteCategory: function(category) {
+        // remove from the channel object
+        /* according to parse docs we should just:*/
+        /* if we remove on parent and relation we get error:
+        Cannot merge Add Op with the previous Op
+        this.channel.remove('Groups', category);
+        */
+        // we also have groupPointer to contend with
+        let relation = this.channel.relation("category");
+        relation.remove(category);
+    
+        //and remove it from this local object
+        let ind = this.categories.findIndex(x => x.id === category.id);
+        this.categories.splice(ind, 1);
+        this.channel.save();  
+        // destroy means delete...
+        /*
+        category.destroy().then((cat) => {
+            // The object was deleted from the Parse Cloud.
+            }, (error) => {
+            // The delete failed.
+            // error is a Parse.Error with an error code and message.
+            // TODO messaging function for unified handling of errors
+            }
+        );
+        */
     },
     addCategory: function (category) {        
         this.categories.push(category);
         //add to the channel object too
-        // can only be done after a save!
-        let channelGroups = this.channel.get('Groups');
-        channelGroups.push(category.id);
-        this.channel.set('Groups', channelGroups);
-        this.channel.set("groupPointer", arrayToPointers(channelGroups, "Group"));
-        this.channel.save();
+        this.channel.add('Groups', category);
+        let relation = this.channel.relation("category");
+        relation.add(category);
+        this.category.save();
     },
     
     addMoji: function(likemoji) {
@@ -47,7 +74,7 @@ const Channel =  {
         likemoji.save();
         this.likemojis.push(likemoji);        
     },
-    removeMoji: function(likemoji) {
+    deleteMoji: function(likemoji) {
         // remove from group(s)
         let rem = this.removeCatMoji;
         this.categories.forEach(function(cat) {
@@ -61,11 +88,19 @@ const Channel =  {
         //TODO save()
     },
     addCatMoji: function(category, likemoji) {
+        /* according to parse docs try
+            let relation = category.relation("likemojis");
+            relation.add(likemoji);
+        */
         let catMojiIDs = category.get('likemojis');
         catMojiIDs.push(likemoji.id);
         category.set('likemojis', catMojiIDs);
     },
     removeCatMoji: function(category, likemoji)  {
+        /* according to parse docs try
+            let relation = category.relation("likemojis");
+            relation.remove(likemoji);
+        */
         // get the array of likemoji pointer from grou
         let catMojiIDs = category.get('likemojis');
         // find the array index of the given likemoji         
@@ -84,7 +119,7 @@ const Channel =  {
         return mainCat[0]; //filter returns array here
     },
     
-    get subCategories() {return this.categories.filter(!isMainCategory)},
+    get subCategories() {return this.categories.filter(function(cat) {return !isMainCategory(cat)})},
     get channelStyle() {return this.styles[0]},
     saveAll: function() {return x}  
 }
@@ -97,23 +132,11 @@ const Channel =  {
     organization.set("header", returnedOrg.attributes.header);
     organization.set("callOut", returnedOrg.attributes.callOut);
     organization.set("editors", editors);
-
-
-org . set("groupPointer", arrayToPointers(groupIDs, "Group"));
+     
+    org . set("groupPointer", arrayToPointers(groupIDs, "Group"));
 						
 
     */
-
-function arrayToPointers(arr, pointerClass) {
-	const pointers = arr.map(id => {
-		return {
-			__type: "Pointer",
-			className: pointerClass,
-			objectId: id
-		};
-	});
-	return pointers;
-}
 
 function isMainCategory(category) {
     return category.attributes.main == 1;
@@ -121,6 +144,8 @@ function isMainCategory(category) {
 
 
 async function getOrgCategories(orgId) {
+    // TODO try to use relations
+    //relation.query().find(
     const query = new Parse.Query("Group");
     query.equalTo("organizationID", orgId);
     query.ascending("order");
@@ -169,11 +194,22 @@ Channel.populate(id)
 // now lets delete the moji from the channel
     let chanMojis = Channel.likemojis;
     console.log(`Channel has ${chanMojis.length} likemojis`); 
-    Channel.removeMoji(testMoji);
+    Channel.deleteMoji(testMoji);
     console.log(`Channel has ${chanMojis.length} likemojis`); 
     console.log('the category without asking has count: ' + mainCatMojis.length);
+    // value not reference....
     mainCatMojis = Channel.catMojis(mainCat);    
-    console.log('the category AFTER asking has count: ' + mainCatMojis.length);
+    console.log('the category after asking has count: ' + mainCatMojis.length);
+    //what I want to be able to do it Channel.categories[0].likemojis 
+    let lastCat = Channel.subCategories[Channel.subCategories.length -1];
+    console.log('found last category named' + lastCat.get('name'));
+    console.log(`Channel has ${Channel.subCategories.length} groups/categories `)
+    console.log('remove the category');
+    Channel.deleteCategory(lastCat);
+    console.log(`Channel now has ${Channel.subCategories.length} groups/categories `)
+    Channel.addCategory(lastCat);
+    console.log(`Channel now has ${Channel.subCategories.length} groups/categories `)
+     
 
 }
 )
