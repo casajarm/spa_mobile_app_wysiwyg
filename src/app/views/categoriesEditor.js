2d17c22489a8fc8b27c65f78585e9eda49c79867
Greg Jarmiolowski <greg@sqlprompt.net>
2019-03-20 09:11:07 -0700

    added sort drag and fixed bug in ul

--- src/app/views/categoriesEditor.js
+++ src/app/views/categoriesEditor.js
@@ -1,91 +1,105 @@
 import {render, html} from '//unpkg.com/lighterhtml?module';
+import {phoneView} from './phoneView.js';
 //.for(categories)
-const categoriesEditorView = categories => {
+
+//const categoriesEditorView = categories => {
+const categoriesEditorView = (Channel) => {
 	let categoriesEditorViewHTML = html`
-		<div id="categoriesEditor" class="editorWindow">
-			<h3>Edit Categories</h3>
-			<hr />
-			<div id="editorContainer" class="image upload">
-				<h4 style="display:inline">Click to edit / Drag to reorder:</h4>
-				<ul id="categoriesInEditor"></ul>
-                ${categories.map((cat, index) => 
+	<div id="categoriesEditor" class="editorWindow">
+		<h3>Edit Categories</h3>
+		<hr />
+		<div id="editorContainer" class="image upload">
+			<h4 style="display:inline">Click to edit / Drag to reorder:</h4>
+			<ul id="categoriesInEditor">
+				${Channel.categories.map((cat, index) => 
 					cat.attributes.main != 1
-						? html`<li
-									id="${cat.id+index}"
-									data-toggle="modal"
-									data-target="#addCategoriesModal"
-                                    class="${cat.attributes.disable == 1 ? 'disable' : ''}"
-                                    >
-									<i class="fas fa-sort"></i>
-									<i class="fas fa-edit floatRight"></i>
-									<p class="inlineDisplay">${cat.attributes.name}</p>
-									<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								</li>`
-						: html`<span></span>`
-                )}
-			</div>
-			<div
-				class="selectDiv text-center"
-				data-toggle="modal"
-				data-target="#addCategoriesModal"
-			>
-				<button
-					type="button"
-					id="addNewCategories"
-					class="btn btn-default addCategories"
-				>
-					<i
-						style="font-size: 1.3em; color: white; margin-top: 4px;"
-						class="far fa-plus-square grow"
-					></i>
-					Add Categories
-				</button>
-			</div>
+					? html`<li
+								id="${cat.id+index}"
+								data-i="${cat.id}"
+								data-toggle="modal"
+								data-target="#addCategoriesModal"
+								class="${cat.attributes.disable == 1 ? 'disable' : ''}"
+								draggable="true"
+								ondrag="${handleDrag}"
+								ondragend="${handleDrop}"
+								>
+								<i class="fas fa-sort"></i>
+								<i class="fas fa-edit floatRight"></i>
+								<p class="inlineDisplay">${cat.attributes.name}</p>
+								<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
+							</li>`
+					: html``
+			)}
+			</ul>
 		</div>
-	`;
-    return categoriesEditorViewHTML;
-}        
-/*
+		<div
+			class="selectDiv text-center"
+			data-toggle="modal"
+			data-target="#addCategoriesModal"
+		>
+			<button
+				type="button"
+				id="addNewCategories"
+				class="btn btn-default addCategories"
+			>
+				<i
+					style="font-size: 1.3em; color: white; margin-top: 4px;"
+					class="far fa-plus-square grow"
+				></i>
+				Add Categories
+			</button>
+		</div>
+	</div>`;
 
-if (groupSelected.attributes.main == 1) {
-	for (var i = 0; i < groups.length; i++) {
-		if (categoriesSorted[i].attributes.main == 1) {
-			// console.log ("skipped");
-		} else {
-			var text = document.createElement("p");
-			var li = document.createElement("li");
-			var span = document.createElement("span");
-			var icon = document.createElement("i");
-			var icon2 = document.createElement("i");
+	function handleDrag(event) {
+		const selectedItem = event.target,
+		list = selectedItem.parentNode,
+		x = event.clientX,
+		y = event.clientY;
 
-			text.innerHTML = categoriesSorted[i].attributes.name;
-			text.setAttribute("class", "inlineDisplay");
+		selectedItem.classList.add('drag-sort-active');
+		let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
 
-			icon.setAttribute("class", "fas fa-sort");
-			icon2.setAttribute("class", "fas fa-edit floatRight");
-
-			span.setAttribute("class", "ui-icon ui-icon-arrowthick-2-n-s");
-			li.setAttribute("id", categoriesSorted[i].id + i);
-			li.setAttribute("data-toggle", "modal");
-			li.setAttribute("data-target", "#addCategoriesModal");
-
-			li.appendChild(icon);
-			li.appendChild(icon2);
-			li.appendChild(text);
-
-			li.appendChild(span);
-
-			if (categoriesSorted[i].attributes.disable == 1) {
-				li.setAttribute("class", "disabledCategory");
-			}
-
-			var element = document.getElementById("categoriesInEditor");
-			element.appendChild(li);
+		if (list === swapItem.parentNode) {
+			swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+			list.insertBefore(selectedItem, swapItem);
 		}
 	}
-} else {
-	console.log("no groups to display");
-}
-*/
+
+	function handleDrop(event) {
+		event.target.classList.remove('drag-sort-active');
+		setNewGroupOrder();
+	}
+
+	//sets group order to order that was set in the dragable editor
+	function setNewGroupOrder() {
+		//user catOrder above to update all groups order
+		let catOrder = document.querySelectorAll('#categoriesInEditor li');	
+		for (var i = 0; i < catOrder.length; i++) {
+			let thisID = catOrder[i].dataset.i;
+			let group = Channel.categories.find(x => x.id === thisID);
+			let order = i + 1;
+			group.set("order", order);
+		}
+		Parse.Object.saveAll(Channel.categories).then(
+			async groupsSaved => {
+				Channel.categories.sort(function (a,b) {
+					return a.get('order') - b.get('order');
+  				});
+				
+				for (var i = 1; i < Channel.categories.length; i++) {
+					console.log(Channel.categories[i].id + " " + Channel.categories[i].attributes.order);
+				}
+				//refresh phone panel here
+				render(panel2, () => phoneView(Channel, Channel.mainCategory.id));
+			},
+			error => {
+				reject();
+				alert("Failed to create new objects, with error code: " + error.message);
+			}
+		);
+	}
+	return categoriesEditorViewHTML;	
+}        
 
 export default categoriesEditorView;
