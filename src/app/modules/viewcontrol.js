c3a04bde6c48d4b9b89e96a212e00c9b391b635a
Greg Jarmiolowski <greg@sqlprompt.net>
2019-04-01 23:11:04 -0600

    new controller for all views on page

---
+++ src/app/modules/viewcontrol.js
@@ -0,0 +1,42 @@
+const {render, html, svg} = lighterhtml;
+const viewControl = {
+    views: []
+};
+
+viewControl.add = function (target, source) {
+    let key = viewControl.views.find( x => x.target === target);
+    if (key) {
+        key.source = source;
+    }
+    else {
+        let viewDef = {target: target, source: source};
+        viewControl.views.push(viewDef);  
+    }  
+    console.log(`added key ${target}`);
+    render(target, source);
+}
+
+viewControl.deleteView = function (target) {
+    let key = viewControl.views.find( x => x.target === target);
+    if (key) {
+        let deleted = viewControl.splice(key);
+        console.log('deleted view' + deleted);
+    }
+    render(target, () => html`<span />`);
+}
+
+viewControl.deleteAll = function () {
+    viewControl.views = [];
+}
+
+viewControl.update = function () {
+    //get object keys
+    //find each target or create if not exists??? TODO should we only allow existing elements here?
+    //and render them
+    //
+    let renders = viewControl.views;
+    console.log(renders);
+    renders.forEach(x => render(x.target, x.source));
+}
+
+export default viewControl;
