a0f9bf0afcb5983f31a28c6e4ec4ff7d41ba4916
Greg Jarmiolowski <greg@sqlprompt.net>
2019-04-08 15:37:05 -0500

    turned jscolor class into variable

--- src/app/views/styleEditorView.js
+++ src/app/views/styleEditorView.js
@@ -1,79 +1,84 @@
 const {render, html, svg} = lighterhtml;
 
 function styleEditorView(Channel) {
+	console.info('rendering stlyeEditorView');
     let style = Channel.channelStyle;
-    //let tempStyle = style.clone();
-    let editorView = html `
-		<div class="modal-body">
-            <div id="colorThemeEditor" class="editorWindow">
-                <div id="themeContainer">
-                    <h4 style="display:inline">Edit Theme (Color / Font):</h4>
-                    <br>
-                    <br>
-                    <div class="form-group jscolor">
-                        <input id="backgroundColor" class="jscolor"
+    let jscolor = `jscolor {position:'right',
+    borderColor:'#FFF #666 #666 #FFF',
+    insetColor:'#666 #FFF #FFF #666',
+    backgroundColor:'#CCC'}`;
+
+    let editorView = html `<div id="colorThemeEditor" class="editorWindow">
+            <div id="themeContainer">
+                <h4 style="display:inline">Edit Theme (Color / Font):</h4>
+                <br>
+                <br>
+                <div class="form-group jscolor">
+                    <label for="backgroundColor">
+                        <input id="backgroundColor" class="${jscolor}"
                             data-attr="generalColor"
                             value="${style.get("generalColor")}"
                             onchange=${setTemp}
                         >
-                        <label for="backgroundColor"> - Background</label>
-                    </div>
-                    <div class="form-group">
-                        <input id="textColor" class="jscolor"
-                            data-attr="generalTextColor"
-                            value="${style.get("generalTextColor")}"
-                            onchange=${setTemp}
-                            >
-                        <label for="textColor"> - Text</label>
-                    </div>
-                    <div class="form-group">
-                        <input id="catTextColor" class="jscolor"
-                            data-attr="catTextColor"
-                            value="${style.get("categoryTextColor")}"
-                            onchange=${setTemp}
+                        - Background</label>
+                </div>
+                <div class="form-group">
+                    <input id="textColor" class="${jscolor}"
+                        data-attr="generalTextColor"
+                        value="${style.get("generalTextColor")}"
+                        onchange=${setTemp}
                         >
-                        <label for="catTextColor"> - Category Label Text</label>
-                    </div>
-                    <div class="form-group">
-                        <input id="navColor" class="jscolor"
-                            data-attr="tabBar starColor navButtonColor"
-                            value="${style.get("tabBar")}"
-                            onchange=${setTemp}
-                        >
-                        <label for="navColor"> - NavBar / Buttons / Stars</label>
-                    </div>
-                    <div class="form-group">
-                        <input id="activeNav" class="jscolor"
-                            data-attr="tabBarSelected"
-                            value="${style.get("tabBarSelected")}"
-                            onchange=${setTemp}
-                        >
-                        <label for="activeNav"> - Active Nav Button</label>
-                    </div>
+                    <label for="textColor"> - Text</label>
+                </div>
+                <div class="form-group">
+                    <input id="catTextColor" class="${jscolor}"
+                        data-attr="catTextColor"
+                        value="${style.get("categoryTextColor")}"
+                        onchange=${setTemp}
+                    >
+                    <label for="catTextColor"> - Category Label Text</label>
+                </div>
+                <div class="form-group">
+                    <input id="navColor" class="${jscolor}"
+                        data-attr="tabBar starColor navButtonColor"
+                        value="${style.get("tabBar")}"
+                        onchange=${setTemp}
+                    >
+                    <label for="navColor"> - NavBar / Buttons / Stars</label>
+                </div>
+                <div class="form-group">
+                    <input id="activeNav" class="${jscolor}"
+                        data-attr="tabBarSelected"
+                        value="${style.get("tabBarSelected")}"
+                        onchange=${setTemp}
+                    >
+                    <label for="activeNav"> - Active Nav Button</label>
                 </div>
             </div>
-    		<div id="themeSaveButtons" class="btn-group themeSaveButtons" role="group" aria-label="...">
-                <button id="editorThemeCancel" class="btn btn-editorThemeSave" onclick="${closeStyleEditor}">
-                    <label for="themeCancel">
-                        <i style="font-size: 1.3em; color: white;" class="fas fa-ban"></i>
-                        <h5 style="display:inline">cancel</h5>
-                    </label>
-                </button>
-                <button id="editorThemeSave"
-                    class="btn btn-editorThemeSave active"
-                    onclick="${saveStyle}"
-                >
-                    <label>
-                            <i style="font-size: 1.3em; color: white;" class="fas fa-save"></i>
-                            <h5 style="display:inline">save</h5>
-                    </label>
-                </button>
-            </div>
-        </div>`;
+        </div>
+        <div id="themeSaveButtons" class="btn-group themeSaveButtons" role="group" aria-label="...">
+            <button id="editorThemeCancel" class="btn btn-editorThemeSave" onclick="${closeStyleEditor}">
+                <label for="themeCancel">
+                    <i style="font-size: 1.3em; color: white;" class="fas fa-ban"></i>
+                    <h5 style="display:inline">cancel</h5>
+                </label>
+            </button>
+            <button id="editorThemeSave"
+                class="btn btn-editorThemeSave active"
+                onclick="${saveStyle}"
+            >
+                <label>
+                        <i style="font-size: 1.3em; color: white;" class="fas fa-save"></i>
+                        <h5 style="display:inline">save</h5>
+                </label>
+            </button>
+        </div>
+        <script>jscolor.installByClassName("jscolor");</script>
+        `;
 
     function setTemp(e) {
         let attribs = e.currentTarget.dataset.attr.split(' ');
-        let val = e.currentTarget.value;
+        let val = '#' + e.currentTarget.value;
         console.log(`set attribute ${attribs} to value ${val}`);
         for (var i = 0; i < attribs.length; i++) {
             style.set(attribs[i], val);
@@ -82,26 +87,16 @@ function styleEditorView(Channel) {
     }
 
     function saveStyle() {
-        /*
-        style.set("organizationID", returnedOrg.id);
-        style.set("generalColor", "#" + $("#backgroundColor").val());
-        style.set("tabBar", "#" + $("#navColor").val());
-        style.set("starColor", "#" + $("#navColor").val());
-        style.set("navButtonColor", "#" + $("#navColor").val());
-        style.set("tabBarSelected", "#" + $("#activeNav").val());
-        style.set("generalTextColor", "#" + $("#textColor").val());
-        style.set("categoryTextColor", "#" + $("#catTextColor").val());
-        */
         //TODO what are these here for???
         style.set("fontCategorySize", "Large");
         style.set("font", "Avenir-Next");
         style.set("fontSize", "16");
 
-        style
-            .save()
-            .then(style => {
+        style.save().then(
+            style => {
                 // Execute any logic that should take place after the object is saved.
                 // alert('Saved objectId: ' + style.id);
+                Channel.updateViewss();
             }, error => {
                 // Execute any logic that should take place if the save fails. error is a
                 // Parse.Error with an error code and message.
@@ -111,7 +106,19 @@ function styleEditorView(Channel) {
 
     function closeStyleEditor (e) {
         e.preventDefault();
-        togglePanelWithin('panel-editor', 'panel3');
+        //refresh Channel data with style from database
+        let close = !style.dirty() || confirm('Discard unsaved changes');
+        if (close) {
+            Channel.resetStyle()
+            .then ( () => {
+                console.info(`reverted stlyle and now tabbar has a value of ${style.get("tabBar")}`);
+                Channel.updateViews();
+                }
+            );
+
+            // TODO is this the best way to revert back?
+            togglePanelWithin('panel-editor', 'panel3');
+        }
     }
 
     return editorView;
