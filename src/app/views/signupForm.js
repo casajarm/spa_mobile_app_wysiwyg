9704169239926f8a4100267f6fe4516694b2c81b
Greg Jarmiolowski <greg@sqlprompt.net>
2019-04-19 15:51:13 -0700

    deleted

---
+++ src/app/views/signupForm.js
@@ -0,0 +1,27 @@
+function signUpForm() {
+    let form = html`<div>
+    <form id="signUpForm" class="form">
+		<h2>Create Your Account</h2>
+		<div class="form-group" id="emaildiv">
+			<label for="emailSignup">email</label>
+			<input type="email" class="form-control" id="inputEmail" placeholder="enter your email here">
+		</div>
+		<div class="form-group" id="passworddiv">
+			<label for="passwordSignup">password</label>
+			<input type="password" class="form-control" id="inputPassword" placeholder="create your password here">
+		</div>
+		<div class="form-group" id="organizationdiv">
+				<label for="organizationSignup">Orgnization Name</label>
+                <input type="text"
+                    class="form-control"
+                    id="inputOrganization"
+                    placeholder="organization name"
+                >
+		</div>
+        <button id="signUpUser2" type="button" class="btn btn-default">Start</button>
+    </form>
+    </div>`;
+
+    return form;
+}
+export default signUpForm;
